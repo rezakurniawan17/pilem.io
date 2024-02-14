f9ae25b2b5c6e1f9692f56ee7880e2d8fe5d3c87
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2024-02-14 20:16:54 +0700

    fix: remove console

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -2,7 +2,6 @@ import { getPopularTVShow } from "@/actions/getPopularTVShow";
 import MovieCard from "@/components/card/movie-card";
 export default async function TVShowPage() {
   const data = await getPopularTVShow();
-  console.log(data);
   return (
     <div className="min-h-screen w-full">
       <div className="grid md:grid-cols-3 grid-cols-2 lg:grid-cols-5 gap-5">
