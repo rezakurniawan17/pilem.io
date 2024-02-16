bb0db6e9b813bc58f184a4a501f2e79c05ad9279
Reza Kurniawan <rezaa.kurniawan17@gmail.com>
2024-02-16 13:20:46 +0700

    edit loading file

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export default function Loading() {
-  return (
-    <div className="w-full h-screen">
-      <div>
-        {...Array(10)
-          .fill(1)
-          .map((_, index) => <Skeleton key={index} />)}
-        <Skeleton />
-      </div>
-    </div>
-  );
+  return <div>Loading...</div>;
 }
